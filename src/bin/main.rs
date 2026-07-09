9d2c3cfc4ce1f0312db6082dd33fce6bd68a2eb9
Aaron Perrotta <19561067+aaronzper@users.noreply.github.com>
2026-07-08 20:20:42 -0400

    feat: Sexy logging

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,17 @@
 use azptui::hello;
+use log::info;
 use ratatui::{
-    Frame, Terminal,
+    Terminal,
     backend::Backend,
-    layout::{Constraint, Layout, Rect},
+    layout::{Constraint, Layout},
+    style::Stylize,
+    text::Line,
+    widgets::Block,
 };
+use tui_logger::{TuiLoggerWidget, init_logger, set_default_level};
 
 fn draw<T: Backend>(t: &mut Terminal<T>) {
+    tui_logger::move_events();
     t.draw(|frame| {
         let layout = Layout::vertical([
             Constraint::Percentage(75),
@@ -13,12 +19,28 @@ fn draw<T: Backend>(t: &mut Terminal<T>) {
         ]);
         let [top, bottom] = layout.areas(frame.area());
         frame.render_widget(hello(), top);
-        frame.render_widget("Will be azptui monitoring, bez\"h", bottom);
+        frame.render_widget(
+            TuiLoggerWidget::default().block(
+                Block::bordered().title("Log").title_bottom(
+                    Line::from(
+                        "Everything outside this box rendered via AZPTUI! B\"SD",
+                    )
+                    .centered()
+                    .italic(),
+                ),
+            ),
+            bottom,
+        );
     })
     .unwrap();
 }
 
 fn main() {
+    init_logger(log::LevelFilter::Trace).unwrap();
+    set_default_level(log::LevelFilter::Trace);
+
+    info!("azptui Demo Started");
+
     ratatui::run(|terminal| {
         loop {
             draw(terminal);
